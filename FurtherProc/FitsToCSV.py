487aef184613b37cad83f5e0f82076bc231c9d2e
Sam D'Ambrosia <Sam.dambrosia@gmail.com>
2020-02-14 11:53:20 -0800

    Added a progress bar to FitsToCSV.py
    Deleted Image.py since DamicImage class was introduced.

--- FurtherProc/FitsToCSV.py
+++ FurtherProc/FitsToCSV.py
@@ -3,11 +3,7 @@ import os
 import re
 import csv
 import time
-
 from tqdm import tqdm
-
-for i in tqdm(range(10)):
-    time.sleep(3)
 
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../AutoAnalysis"))
 import PixelDistribution as pd
@@ -31,32 +27,44 @@ def imageToDict(filepath):
     reverseHistogram = (1, 0)["Avg" in filepath]
     image = DamicImage.DamicImage(data[:, :, -1], reverse=reverseHistogram)
     
+    #with tqdm(range(9)) as pbar:
+
     dictImage = {}
-    
+
     ### Define name, skips, size
-    
+
     dictImage['imgName'] = os.path.basename(filepath)
     dictImage['NDCMS'] = header['NDCMS']
     dictImage['NAXIS1'] = header['NAXIS1']
     dictImage['NAXIS2'] = header['NAXIS2']
-    
+
+#    pbar.update(1)
+
     ### Define output variables
-    
+
     #find image noise
     dictImage['imgNoise'] = pd.computeImageNoise(data[:, :, :-1])
-    
+
+   # pbar.update(1)
+
     #find individual peak noise
     nSmoothing = 4 if int(header['NDCMS']) > 1 else 12 # need less agressive moving average on skipper images
     skImageNoise, skImageNoiseErr = pd.computeSkImageNoise(image, nMovingAverage=nSmoothing)
     dictImage['skNoise'] = pd.convertValErrToString((skImageNoise, skImageNoiseErr))
-    
+
+ #   pbar.update(1)
+
     #find entropy of the average image
     dictImage['aveImgS'] = pd.imageEntropy(data[:, :, -1])
-    
+
+ #   pbar.update(1)
+
     #find rate of entropy change as a function of skips
     entropySlope, entropySlopeErr, _ = pd.imageEntropySlope(data[:, :, :-1])
     dictImage['dSdskip'] = pd.convertValErrToString((entropySlope, entropySlopeErr))
-    
+
+#    pbar.update(1)
+
     #find dark current
     if header['NDCMS'] > 1000:
         darkCurrent, darkCurrentErr = pd.computeDarkCurrent(
@@ -65,34 +73,42 @@ def imageToDict(filepath):
         dictImage['DC'] = pd.convertValErrToString((darkCurrent, darkCurrentErr))
     else:
         dictImage['DC'] = -1
-    
-    
+
+#    pbar.update(1)
+
+
     # Compute pixel noise metrics
     ntrials = 10000
     singlePixelVariance, _ = ps.singlePixelVariance(data[:, :, :-1], ntrials=ntrials)
     imageNoiseVariance, _ = ps.imageNoiseVariance(
         data[:, :, :-1], header['NDCMS'] - c.SKIPPER_OFFSET, ntrials=ntrials
     )
-    
+
+#    pbar.update(1)
+
     #find variance of random pixels
     dictImage['pixVar'] = singlePixelVariance
-    
+
     #find variance of random clusters of pixels
     dictImage['clustVar'] = imageNoiseVariance
-    
+
     #find tail ratio
     dictImage['tailRatio'] = pd.computeImageTailRatio(image)
-    
+
+    #pbar.update(1)
+
     ### Define input variables
-    
-    headervars = ['EXP', 'AMPL', 'HCKDRIN', 'VCKDRIN', 'ITGTIME', 'VIDGAIN', 'PRETIME', 'POSTIME', 'DGWIDTH', 'RGWIDTH', 'OGWIDTH', 'SWWIDTH', 'HWIDTH', 'HOWIDTH', 'VWIDTH', 'VOWIDTH', 'ONEVCKHI', 'ONEVCKLO', 'TWOVCKHI', 'TWOVCKLO', 'TGHI', 'TGLO', 'HUHI', 'HULO', 'HLHI', 'HLLO', 'RGHI', 'RGLO', 'SWLO', 'DGHI', 'DGLO', 'OGHI', 'OGLO', 'BATTR', 'VDD1', 'VDD2', 'DRAIN1', 'DRAIN2', 'VREF1', 'VREF2', 'OPG1', 'OPG2']
-    
+
+    headervars = ['EXP', 'AMPL', 'HCKDIRN', 'VCKDIRN', 'ITGTIME', 'VIDGAIN', 'PRETIME', 'POSTIME', 'DGWIDTH', 'RGWIDTH', 'OGWIDTH', 'SWWIDTH', 'HWIDTH', 'HOWIDTH', 'VWIDTH', 'VOWIDTH', 'ONEVCKHI', 'ONEVCKLO', 'TWOVCKHI', 'TWOVCKLO', 'TGHI', 'TGLO', 'HUHI', 'HULO', 'HLHI', 'HLLO', 'RGHI', 'RGLO', 'SWLO', 'DGHI', 'DGLO', 'OGHI', 'OGLO', 'BATTR', 'VDD1', 'VDD2', 'DRAIN1', 'DRAIN2', 'VREF1', 'VREF2', 'OPG1', 'OPG2']
+
     for var in headervars:
         try:
             dictImage[var] = header[var]
         except:
             dictImage[var] = -1
-    
+
+    #pbar.update(1)
+
     return dictImage
     
     
@@ -109,26 +125,41 @@ def main(directory):
 #dictImage                      
                       'imgNoise', 'skNoise', 'aveImgS', 'dSdskip', 'pixVar', 'clustVar', 'tailRatio', 'DC',
 #inputs
-                      'EXP', 'AMPL', 'HCKDRIN', 'VCKDRIN', 'ITGTIME', 'VIDGAIN', 'PRETIME', 'POSTIME', 'DGWIDTH', 'RGWIDTH', 'OGWIDTH', 'SWWIDTH', 'HWIDTH', 'HOWIDTH', 'VWIDTH', 'VOWIDTH', 'ONEVCKHI', 'ONEVCKLO', 'TWOVCKHI', 'TWOVCKLO', 'TGHI', 'TGLO', 'HUHI', 'HULO', 'HLHI', 'HLLO', 'RGHI', 'RGLO', 'SWLO', 'DGHI', 'DGLO', 'OGHI', 'OGLO', 'BATTR', 'VDD1', 'VDD2', 'DRAIN1', 'DRAIN2', 'VREF1', 'VREF2', 'OPG1', 'OPG2']
+                      'EXP', 'AMPL', 'HCKDIRN', 'VCKDIRN', 'ITGTIME', 'VIDGAIN', 'PRETIME', 'POSTIME', 'DGWIDTH', 'RGWIDTH', 'OGWIDTH', 'SWWIDTH', 'HWIDTH', 'HOWIDTH', 'VWIDTH', 'VOWIDTH', 'ONEVCKHI', 'ONEVCKLO', 'TWOVCKHI', 'TWOVCKLO', 'TGHI', 'TGLO', 'HUHI', 'HULO', 'HLHI', 'HLLO', 'RGHI', 'RGLO', 'SWLO', 'DGHI', 'DGLO', 'OGHI', 'OGLO', 'BATTR', 'VDD1', 'VDD2', 'DRAIN1', 'DRAIN2', 'VREF1', 'VREF2', 'OPG1', 'OPG2']
+        '''
+from tqdm import tqdm
+
+for i in tqdm(range(10)):
+    time.sleep(3)
+'''
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         
-        #iterate through all files in the specified directory and its subdirectories
+        #see how many images are in the directory
+        
+        imgs = 0
+        
         for subdir, dirs, files in os.walk(directory):
             for image in files:
-                
-                filepath = subdir + os.sep + image
-                
-                if(ImgForm.match(image)):
-                    writer.writerow(imageToDict(filepath))
-                    
+                imgs += 1
         
+        #iterate through all files in the specified directory and its subdirectories
+        with tqdm(range(imgs)) as pbar:
+            for subdir, dirs, files in os.walk(directory):
+                for image in files:
+                    
+                    pbar.update(1)
+
+                    filepath = subdir + os.sep + image
+
+                    if(ImgForm.match(image)):
+                        writer.writerow(imageToDict(filepath))
+                
     
 #Check correct formatting for input directory
 
 try:
     directory = sys.argv[1]
-    print(os.path.basename(directory))
     if not (os.path.isdir(directory)):
         print("Cannot find directory!")
         assert(os.path.isdir(directory))
