b7432685a08647669150e6c0749a44b665dfeab7
Sam D'Ambrosia <Sam.dambrosia@gmail.com>
2020-02-26 18:32:11 -0800

    Updated Graphing.py
    Made bounds for variable graphing, allowed for use in notebook.

--- FurtherProc/Graphing.py
+++ FurtherProc/Graphing.py
@@ -3,7 +3,7 @@ import sys
 import csv
 import numpy as np
 
-def graphStoO(setting, output, imgcsv):
+def graph1D(imgcsv, setting, smin, smax, output, omin, omax):
     '''
     Generates a graph showing relationship between one setting var and one output var
     '''
@@ -17,28 +17,15 @@ def graphStoO(setting, output, imgcsv):
             
             #for key in img
             
-            sets.append(float(img[str(setting)]))
-            outs.append(float(img[str(output)]))
-    
-
-    x = np.array(sets)
-    y = np.array(outs)
+            if((smin < float(img[str(setting)]) < smax) and (smin < float(img[str(output)]) < omax)):
+                sets.append(float(img[str(setting)]))
+                outs.append(float(img[str(output)]))
     
     colors = 'blue'
     area = 7
 
-    plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+    plt.scatter(sets, outs, s=area, c=colors, alpha=0.5)
     plt.title(setting + " to " + output)
     plt.xlabel(setting)
     plt.ylabel(output)
     plt.show()
-    
-def main(params):
-    
-    imgcsv = sys.argv[1]
-    setting = sys.argv[2]
-    output = sys.argv[3]
-    
-    graphStoO(setting, output, imgcsv)
-
-main(sys.argv)
